472a1af5a5f0c8a27655ac960789bbd2d33c922b
Timo Vlot <timo.vlot@sqa.org.uk>
2023-09-07 11:36:00 +0100

    Forgot to commit proxmox-vm main.tf

---
+++ terraform/modules/proxmox-vm/main.tf
@@ -0,0 +1,36 @@
+resource "proxmox_vm_qemu" "k8s-master" {
+  count       = 3
+  name        = "k8s0${count.index + 1}.mydomain.intra"
+  target_node = var.proxmox_host
+  vmid        = "101${count.index + 1}"
+  clone       = var.template_name
+  os_type     = "cloud-init"
+  cpu         = "kvm64"
+  cores       = 2
+  sockets     = 1
+  memory      = 6144
+  scsihw      = "virtio-scsi-pci"
+  bootdisk    = "scsi0"
+
+  disk {
+    slot     = 0
+    size     = "50G"
+    type     = "scsi"
+    storage  = "zfs-vm-etc"
+    iothread = 1
+  }
+
+  network {
+    model     = "virtio"
+    bridge    = "vmbr0"
+    tag       = 101
+    firewall  = false
+    link_down = false
+  }
+
+  ipconfig0 = "ip=192.168.10.1${count.index + 1}/22,gw=192.168.10.1"
+  ciuser    = "terraform"
+  sshkeys   = <<EOF
+  ${var.ssh_key}
+  EOF
+}
