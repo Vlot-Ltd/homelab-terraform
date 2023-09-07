70efa75c4060f46b59800d9ea5e4fd038ed0cea2
Timo Vlot <timo.vlot@sqa.org.uk>
2023-09-07 11:34:59 +0100

    Added base config for proxmox-vm module

---
+++ terraform/modules/proxmox-vm/provider.tf
@@ -0,0 +1,6 @@
+provider "proxmox" {
+  pm_api_url = var.proxmox_api_url
+  pm_api_token_id = var.proxmox_user
+  pm_api_token_secret = var.proxmox_token
+  pm_tls_insecure     = var.proxmox_ignore_tls
+}
