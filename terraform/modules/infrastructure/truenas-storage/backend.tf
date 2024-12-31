60749b2428b4a029b8e507f12f709a8862fdf893
Timo Vlot <timo@vlot.org.uk>
2024-12-31 13:00:21 +0000

    Found more API keys. Whoops

--- terraform/modules/infrastructure/truenas-storage/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source = "dariusbakunas/truenas"
-      version = "0.11.1"
-    }
-  }
-}
--- terraform/modules/infrastructure/truenas-storage/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "truenas" {
-  api_key = var.truenas_apikey
-  base_url = var.truenas_apiurl
-}
